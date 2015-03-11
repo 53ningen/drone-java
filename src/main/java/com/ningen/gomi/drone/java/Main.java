5e7168724a252bac04ebedd0a4a8d523f98ac877
53nigen <korun24276@gmail.com>
2015-03-11 20:55:11 +0900

    initial commit

---
+++ src/main/java/com/ningen/gomi/drone/java/Main.java
@@ -0,0 +1,14 @@
+package com.ningen.gomi.drone.java;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+
+    }
+
+    public static int getOne() {
+        return 1;
+    }
+
+}
